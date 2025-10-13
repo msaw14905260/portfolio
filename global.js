dda2afd1e5e67f0e81beb5853e672ae00c3c369e
Marianne Sawires <masawire@ucsd.edu>
2025-10-13 13:45:28 -0700

    Update global.js

--- global.js
+++ global.js
@@ -4,33 +4,48 @@ function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
 
+// let navLinks = $$("nav a"); 
+// let currentLink = navLinks.find(
+// (a) => a.host === location.host && a.pathname === location.pathname,
+// );
+// currentLink?.classList.add('current');
+
 const BASE_PATH =
   location.hostname === 'localhost' || location.hostname === '127.0.0.1'
     ? '/'
     : '/portfolio/';
 
 let pages = [
-  { url: '', title: 'Home' },
-  { url: 'projects/', title: 'Projects' },
-  { url: 'CV/', title: 'CV' },
-  { url: 'contact/', title: 'Contact' }
+  { url: "", title: "Home" },
+  { url: "projects/", title: "Projects" },
+  { url: "contact/", title: "Contact" },
+  { url: "https://github.com/msaw14905260", title: "GitHub" },
+  { url: "CV/", title: "CV" },
 ];
 
-let nav = document.createElement('nav');
+let nav = document.createElement("nav");
 document.body.prepend(nav);
 
 for (let p of pages) {
   let url = p.url;
   let title = p.title;
 
-  if (!url.startsWith('http')) {
+  if (!url.startsWith("http")) {
     url = BASE_PATH + url;
   }
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
-}
 
-let navLinks = $$("nav a");
-let currentLink = navLinks.find(
-  (a) => a.host === location.host && a.pathname === location.pathname
-);
-currentLink?.classList.add('current');
+  let a = document.createElement("a");
+  a.href = url;
+  a.textContent = title;
+
+  a.classList.toggle(
+    "current",
+    a.host === location.host && a.pathname === location.pathname
+  );
+
+  if (a.host !== location.host) {
+    a.target = "_blank";
+  }
+
+  nav.append(a);
+}
