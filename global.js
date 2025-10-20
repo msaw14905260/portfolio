1f7cedbb4b65d9a2f01c03be5118887659dc3582
Marianne Sawires <masawire@ucsd.edu>
2025-10-20 13:50:03 -0700

    Update global.js

--- global.js
+++ global.js
@@ -94,3 +94,23 @@ form?.addEventListener('submit', (event) => {
 
   location.href = url;
 });
+
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+
+    console.log(response);
+
+    const data = await response.json();
+    return data;
+
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
