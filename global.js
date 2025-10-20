93fdbe8555381fef662f0616e471638971c3e9e8
Marianne Sawires <masawire@ucsd.edu>
2025-10-20 14:33:51 -0700

    lab 4, step 1.5

--- global.js
+++ global.js
@@ -114,3 +114,18 @@ export async function fetchJSON(url) {
   }
 }
 
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+
+  for (const project of projects) {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <${headingLevel}>${project.title}</${headingLevel}>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  }
+}
