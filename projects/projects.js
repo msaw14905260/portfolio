3b6ad2323e6a7392eb36d4dee0ea60a0dcbe674f
Marianne Sawires <masawire@ucsd.edu>
2025-10-31 13:59:15 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -6,14 +6,15 @@ const container = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
 document.querySelector('.projects-title').textContent = `Projects (${projects.length})`;
 
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
+
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 
 let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
