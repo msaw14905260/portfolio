8de90109cbf2dc9725a42bade19c32faff39581a
Marianne Sawires <masawire@ucsd.edu>
2025-10-31 13:13:31 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -12,9 +12,30 @@ const titleElement = document.querySelector('.projects-title');
 titleElement.textContent = `Projects (${projects.length})`;
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let data = [1, 2];
+
+let total = 0;
+for (let d of data) {
+  total += d;
+}
+
 let arc = arcGenerator({
   startAngle: 0,
   endAngle: 2 * Math.PI,
 });
 
-d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+let angle = 0;
+let arcData = [];
+
+for (let d of data) {
+  let endAngle = angle + (d / total) * 2 * Math.PI;
+  arcData.push({ startAngle: angle, endAngle });
+  angle = endAngle;
+}
+
+let arcs = arcData.map((d) => arcGenerator(d));
+
+arcs.forEach((arc) => {
+  d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
+});
