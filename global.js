8af8ca5afb618df5936aedb598b70504fe8f8880
Marianne Sawires <masawire@ucsd.edu>
2025-10-31 12:42:59 -0700

    lab 5, step 0

--- global.js
+++ global.js
@@ -123,7 +123,10 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     article.innerHTML = `
       <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
-      <p>${project.description}</p>
+      <div class="project-text">
+        <p class="project-description">${project.description}</p>
+        ${project.year ? `<p class="project-year" aria-label="Project year">${project.year}</p>` : ''}
+      </div>
     `;
 
     containerElement.appendChild(article);
