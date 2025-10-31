6d06d854c6eb8a4f983d4022b45f451ec29fa14e
Marianne Sawires <masawire@ucsd.edu>
2025-10-31 13:24:34 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,43 +1,24 @@
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-
 import { fetchJSON, renderProjects } from '../global.js';
 
 const projects = await fetchJSON('../lib/projects.json');
+const container = document.querySelector('.projects');
+renderProjects(projects, container, 'h2');
+document.querySelector('.projects-title').textContent = `Projects (${projects.length})`;
 
-const projectsContainer = document.querySelector('.projects');
-
-renderProjects(projects, projectsContainer, 'h2');
-
-const titleElement = document.querySelector('.projects-title');
-titleElement.textContent = `Projects (${projects.length})`;
+let data = [1, 2];
+let colors = ['gold', 'purple'];
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let data = [1, 2];
-
-let total = 0;
-for (let d of data) {
-  total += d;
-}
-
-let arc = arcGenerator({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
-
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
 
-let colors = ['gold', 'purple'];
-
 arcs.forEach((arc, idx) => {
-  d3.select('svg').append('path').attr('d', arc).attr('fill', colors[idx]);
+  d3.select('svg')
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
 });
