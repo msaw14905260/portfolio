71c9dd08520f2355952a5c6fdde5d2fe277dd3a0
Marianne Sawires <masawire@ucsd.edu>
2025-10-31 13:41:05 -0700

    lab 5, step 2

--- projects/projects.js
+++ projects/projects.js
@@ -6,12 +6,21 @@ const container = document.querySelector('.projects');
 renderProjects(projects, container, 'h2');
 document.querySelector('.projects-title').textContent = `Projects (${projects.length})`;
 
-let data = [1, 2];
-let colors = ['gold', 'purple'];
+let data = [
+  { value: 1, label: 'apples' },
+  { value: 2, label: 'oranges' },
+  { value: 3, label: 'mangos' },
+  { value: 4, label: 'pears' },
+  { value: 5, label: 'limes' },
+  { value: 5, label: 'cherries' },
+];
+
+let colors = d3.scaleOrdinal(d3.schemeTableau10);
 
 let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
 
-let sliceGenerator = d3.pie();
+let sliceGenerator = d3.pie().value((d) => d.value);
+
 let arcData = sliceGenerator(data);
 
 let arcs = arcData.map((d) => arcGenerator(d));
@@ -20,5 +29,13 @@ arcs.forEach((arc, idx) => {
   d3.select('svg')
     .append('path')
     .attr('d', arc)
-    .attr('fill', colors[idx]);
+    .attr('fill', colors(idx));
+});
+
+let legend = d3.select('.legend');
+data.forEach((d, idx) => {
+  legend.append('li')
+    .attr('class', 'legend-item')
+    .attr('style', `--color:${colors(idx)}`)
+    .html(`<span class="swatch"></span> ${d.label} <em>(${d.value})</em>`);
 });
