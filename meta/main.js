f9be603e6874f71a6480b2d1e21f4a8bfc882832
Marianne Sawires <masawire@ucsd.edu>
2025-11-06 20:38:24 -0800

    step 3

--- meta/main.js
+++ meta/main.js
@@ -117,39 +117,45 @@ function renderScatterPlot(data, commits) {
     .domain([0, 24])
     .range([usableArea.bottom, usableArea.top]);
 
+  const gridlines = svg
+    .append('g')
+    .attr('class', 'gridlines')
+    .attr('transform', `translate(${usableArea.left}, 0)`);
+  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
+
+  const xAxis = d3.axisBottom(xScale);
+  const yAxis = d3.axisLeft(yScale).tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
+
+  svg.append('g')
+    .attr('transform', `translate(0, ${usableArea.bottom})`)
+    .call(xAxis);
+
+  svg.append('g')
+    .attr('transform', `translate(${usableArea.left}, 0)`)
+    .call(yAxis);
+
   const dots = svg.append('g').attr('class', 'dots');
 
-  dots
-    .selectAll('circle')
+  dots.selectAll('circle')
     .data(commits)
     .join('circle')
     .attr('cx', (d) => xScale(d.datetime))
     .attr('cy', (d) => yScale(d.hourFrac))
     .attr('r', 5)
-    .attr('fill', 'steelblue');
-
-  const gridlines = svg
-  .append('g')
-  .attr('class', 'gridlines')
-  .attr('transform', `translate(${usableArea.left}, 0)`);
-  
-  gridlines.call(d3.axisLeft(yScale).tickFormat('').tickSize(-usableArea.width));
-
-  const xAxis = d3.axisBottom(xScale);
-  const yAxis = d3
-    .axisLeft(yScale)
-    .tickFormat((d) => String(d % 24).padStart(2, '0') + ':00');
-
-  svg
-    .append('g')
-    .attr('transform', `translate(0, ${usableArea.bottom})`)
-    .call(xAxis);
-
-  svg
-    .append('g')
-    .attr('transform', `translate(${usableArea.left}, 0)`)
-    .call(yAxis);
+    .attr('fill', 'steelblue')
+    .on('mouseenter', (event, commit) => {
+      renderTooltipContent(commit);
+    })
+    .on('mouseleave', () => {
+      document.getElementById('commit-link').textContent = '';
+      document.getElementById('commit-link').removeAttribute('href');
+      document.getElementById('commit-date').textContent = '';
+      document.getElementById('commit-time').textContent = '';
+      document.getElementById('commit-author').textContent = '';
+      document.getElementById('commit-lines').textContent = '';
+    });
 }
+
 
 function renderTooltipContent(commit) {
   const link = document.getElementById('commit-link');
